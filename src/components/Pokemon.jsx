7df34b1f3b866ed2cbaa4829a7d4b8b0a0bc555e
lepfau <edouard.pfauwadel@gmail.com>
2021-02-26 18:26:49 +0100

    first try to change style according to pok type

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -3,7 +3,17 @@ import axios from 'axios';
 export class Pokemon extends Component {
 
 state= {
-    sprites: []
+    sprites: [],
+    id: 0,
+    type: ""
+}
+
+typeClass() {
+    if(this.state.type === "fire")
+    return 'pokemonimgfire'
+    else if(this.state.type === "water")
+    return 'pokemonimgwater'
+    else return "pokemonimg"
 }
 
 componentDidMount() {
@@ -12,8 +22,9 @@ componentDidMount() {
       .then((responseFromApi) => {
         // console.log(responseFromApi.data.sprites.version['generation-v']['black-white'].animated.back_default);
         this.setState({
-          
-          sprites: responseFromApi.data.sprites.versions["generation-v"]['black-white'].animated.front_default
+          id: responseFromApi.data.id,
+          sprites: responseFromApi.data.sprites.versions["generation-v"]['black-white'].animated.front_default,
+          type: responseFromApi.data.types[0].type.name
         });
       });
   }
@@ -23,7 +34,9 @@ componentDidMount() {
         return (
             <div className="pokemonCard">
                 <p>{this.props.name.toUpperCase()}</p>
-                <img className="pokemonimg" alt="pokemonimg" src={this.state.sprites}/>
+                <p>{this.state.type}</p>
+                <img className={this.typeClass()} alt="pokemonimg" src={this.state.sprites}/>
+                <p>#{this.state.id}</p>
             </div>
         )
     }
